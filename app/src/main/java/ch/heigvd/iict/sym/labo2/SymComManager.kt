6c8a08420627278f35da96e151ff3c0228d3b2ca
Julien Béguin <julien.beguin99@gmail.com>
2020-10-08 16:14:13 +0200

    sendRequest test

--- app/src/main/java/ch/heigvd/iict/sym/labo2/SymComManager.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/SymComManager.kt
@@ -1,9 +1,28 @@
 package ch.heigvd.iict.sym.lab.comm
 
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
 class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
 
     fun sendRequest(url: String, request: String) {
-		//TODO to implement
-    }
+        val url = URL("http://www.android.com/")
+        val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+        try {
+            val input: InputStream = BufferedInputStream(urlConnection.getInputStream())
+            val isw = InputStreamReader(input)
 
+            var data = isw.read()
+            while (data != -1) {
+                val current = data.toChar()
+                data = isw.read()
+                print(current)
+            }
+        } finally {
+            urlConnection.disconnect()
+        }
+    }
 }

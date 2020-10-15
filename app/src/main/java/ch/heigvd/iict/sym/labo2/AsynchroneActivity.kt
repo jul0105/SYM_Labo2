2fe19285a995728b3b36ac8d0cf275c0a3b99529
Julien Béguin <julien.beguin99@gmail.com>
2020-10-15 16:10:21 +0200

    send request

--- app/src/main/java/ch/heigvd/iict/sym/labo2/AsynchroneActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/AsynchroneActivity.kt
@@ -2,14 +2,17 @@ package ch.heigvd.iict.sym.labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ch.heigvd.iict.sym.lab.comm.SymComManager
 
-class AsynchroneActivity : AppCompatActivity() {
+class AsynchroneActivity : AppCompatActivity(), CommunicationEventListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asynchrone)
 
-        val sm = SymComManager()
-        sm.sendRequest("https://enlidghzzpq3.x.pipedream.net/", "")
+        val sm = SymComManager(this)
+        sm.sendRequest("https://eng5abs5srkxm.x.pipedream.net", "")
+    }
+
+    override fun handleServerResponse(response: String) {
+        println(response)
     }
 }
--- app/src/main/java/ch/heigvd/iict/sym/labo2/SymComManager.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/SymComManager.kt
@@ -1,4 +1,4 @@
-package ch.heigvd.iict.sym.lab.comm
+package ch.heigvd.iict.sym.labo2
 
 import java.io.BufferedInputStream
 import java.io.InputStream
@@ -8,20 +8,28 @@ import java.net.URL
 
 class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
 
-    fun sendRequest(url: String, request: String) {
-        val urlConnection: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
-        try {
-            val input: InputStream = BufferedInputStream(urlConnection.getInputStream())
-            val isw = InputStreamReader(input)
+//    fun sendRequest(url: String, request: String) {
+//        val urlConnection: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
+//        try {
+//            val input: InputStream = BufferedInputStream(urlConnection.inputStream)
+//            val isw = InputStreamReader(input)
+//
+//            var data = isw.read()
+//            while (data != -1) {
+//                val current = data.toChar()
+//                data = isw.read()
+//                print(current)
+//            }
+//        } finally {
+//            urlConnection.disconnect()
+//        }
+//    }
 
-            var data = isw.read()
-            while (data != -1) {
-                val current = data.toChar()
-                data = isw.read()
-                print(current)
+    fun sendRequest(url: String, request: String) {
+        object : Thread() {
+            override fun run() {
+                communicationEventListener?.handleServerResponse(URL(url).readText())
             }
-        } finally {
-            urlConnection.disconnect()
-        }
+        }.start()
     }
 }

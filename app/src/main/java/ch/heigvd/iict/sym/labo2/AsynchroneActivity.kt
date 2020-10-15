2c0fe21387233bd960eeb5a3ef78370de987bf89
robincuenoud <robin.cuenoud@heig-vd.ch>
2020-10-15 16:17:41 +0200

    added constraints

--- app/src/main/java/ch/heigvd/iict/sym/labo2/AsynchroneActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/AsynchroneActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ch.heigvd.iict.sym.lab.comm.SymComManager
 
-class AsynchroneActivity : AppCompatActivity() {
+class AsynchroneActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asynchrone)

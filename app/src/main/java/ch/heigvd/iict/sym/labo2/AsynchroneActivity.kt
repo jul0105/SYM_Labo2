b720a9faae474819ff35ede183a78df9d9db7a28
robincuenoud <robin.cuenoud@heig-vd.ch>
2020-11-10 15:35:37 +0100

    asynchrone works

--- app/src/main/java/ch/heigvd/iict/sym/labo2/AsynchroneActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/AsynchroneActivity.kt
@@ -2,19 +2,40 @@ package ch.heigvd.iict.sym.labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 
 class AsynchroneActivity : AppCompatActivity(), CommunicationEventListener {
 
+    private lateinit var send_button: Button;
+    private lateinit var text_input: EditText;
+    private lateinit var received_text: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_asynchrone)
 
+        send_button = findViewById(R.id.send_button);
+        text_input = findViewById(R.id.send_text);
+        received_text = findViewById(R.id.received_text);
+
+
+
         val sm = SymComManager(this)
-        sm.sendRequest("http://sym.iict.ch/rest/txt/", "les gros boloss")
+
+
+        send_button.setOnClickListener {
+            sm.sendRequest("http://sym.iict.ch/rest/txt/", text_input.text.toString());
+
+        }
     }
 
     override fun handleServerResponse(response: String) {
-        println(response)
+       received_text.text = response;
     }
+
+
 }

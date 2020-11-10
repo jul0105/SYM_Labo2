8e713d650067f17a16e72de7c3aa6c1a97620dd3
robincuenoud <robin.cuenoud@heig-vd.ch>
2020-11-10 16:03:02 +0100

    ca avance

--- app/src/main/java/ch/heigvd/iict/sym/labo2/DiffereeActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/DiffereeActivity.kt
@@ -1,11 +1,28 @@
 package ch.heigvd.iict.sym.labo2
 
+import android.app.job.JobInfo
+import android.app.job.JobService
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.util.stream.DoubleStream.builder
 
 class DiffereeActivity : AppCompatActivity() {
+    private lateinit var send_button: Button;
+    private lateinit var text_input: EditText;
+    private lateinit var received_text: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_differee)
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_asynchrone)
+
+        send_button = findViewById(R.id.send_button);
+        text_input = findViewById(R.id.send_text);
+        received_text = findViewById(R.id.received_text);
+
+        //JobInfo.Builder b = new JobInfo.Builder(1,new ComponentName(this,MyJobService.class))
     }
 }

3c3d1b06e16dc8f7d010f6f1f7eb4c87935266bf
Julien Béguin <julien.beguin99@gmail.com>
2020-11-14 18:31:52 +0100

    compression ok

--- app/src/main/java/ch/heigvd/iict/sym/labo2/CompressedActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/CompressedActivity.kt
@@ -2,10 +2,39 @@ package ch.heigvd.iict.sym.labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class CompressedActivity : AppCompatActivity() {
+
+class CompressedActivity : AppCompatActivity(), CommunicationEventListener {
+
+    private lateinit var send_button: Button;
+    private lateinit var text_input: EditText;
+    private lateinit var received_text: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_compressed)
+
+        send_button = findViewById(R.id.send_button);
+        text_input = findViewById(R.id.send_text);
+        received_text = findViewById(R.id.received_text);
+
+
+
+        val sm = SymComManager(this)
+
+
+        send_button.setOnClickListener {
+            sm.sendRequest("http://sym.iict.ch/rest/txt/", text_input.text.toString(), compressed = true);
+        }
     }
+
+    override fun handleServerResponse(response: String) {
+        received_text.text = response;
+    }
+
+
 }

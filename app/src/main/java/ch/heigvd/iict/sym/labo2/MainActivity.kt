8444795b2270b1592026d6796e741846bdb2107a
Julien Béguin <julien.beguin99@gmail.com>
2020-11-15 19:58:29 +0100

    ajout des commentaires de fichiers

--- app/src/main/java/ch/heigvd/iict/sym/labo2/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/MainActivity.kt
@@ -1,3 +1,10 @@
+/**
+ * SYM : Labo 2 - Protocoles applicatifs
+ * Auteurs : Julien Béguin, Robin Cuénoud & Gaëtan Daubresse
+ * Date : 15.11.2020
+ * Classe : B
+ */
+
 package ch.heigvd.iict.sym.labo2
 
 import android.content.Intent
